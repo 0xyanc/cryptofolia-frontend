86e61e452d04efdd20bcc768a5e6a6c00ac8b950
Philipp Hiestand <hiestand@dinotronic.ch>
2023-04-15 12:16:56 +0200

    ImageSlicer in two version

---
+++ components/ImageSlicer/ImageSlicerBuildUp.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+
+function ImageSlicer() {
+  const [showImage, setShowImage] = useState(false);
+  const [lastSliceY, setLastSliceY] = useState(null);
+
+  function handleClick() {
+    setShowImage(true);
+    const canvas = document.getElementById('canvas');
+    const ctx = canvas.getContext('2d');
+    const img = new Image();
+    img.onload = () => {
+      ctx.drawImage(img, 0, 0);
+      setLastSliceY(0);
+    };
+  }
+
+  function handleSlice() {
+    const canvas = document.getElementById('canvas');
+    const ctx = canvas.getContext('2d');
+    const img = new Image();
+    img.src = 'https://www.w3schools.com/w3images/wedding.jpg';
+    img.onload = () => {
+      const sliceHeight = img.height / 10;
+      let y;
+      if (lastSliceY === null) {
+        y = sliceHeight;
+      } else {
+        y = lastSliceY + sliceHeight;
+      }
+      ctx.drawImage(img, 0, y, img.width, sliceHeight, 0, y, img.width, sliceHeight);
+      setLastSliceY(y);
+    };
+  }
+
+  return (
+    <div>
+      <canvas id="canvas" width="600" height="400"></canvas>
+      <button onClick={handleClick}>Show Image</button>
+      {showImage && (
+        <div>
+          <button onClick={handleSlice}>Add Slice</button>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default ImageSlicer;
